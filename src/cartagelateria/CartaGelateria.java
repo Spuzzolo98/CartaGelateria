57a41e6300073157a2c01366c88dc9487a054b57
Lorenzo <60008393+Spuzzolo98@users.noreply.github.com>
2021-01-02 21:42:36 +0100

    Modifiicato il codice
    ho aggiunto degli spazi

--- src/cartagelateria/CartaGelateria.java
+++ src/cartagelateria/CartaGelateria.java
@@ -8,7 +8,11 @@ public class CartaGelateria {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+
+
+
+
     }
     
 }
