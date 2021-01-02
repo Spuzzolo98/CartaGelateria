6e8eb5d6250cae7b74e0cf8e89c21a1bda4f410c
Lorenzo <60008393+Spuzzolo98@users.noreply.github.com>
2021-01-02 23:44:44 +0100

    Lavoro in main
    Finiti i primi due putni, iniziato il terzo

--- src/ClassiVarie/Carta.java
+++ src/ClassiVarie/Carta.java
@@ -1,14 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ClassiVarie;
 
-/**
- *
- * @author loren
- */
+
 public class Carta {
+    private String data="", nomeutente, cognomeutente;
+    private int giornor, meser, annor, codiceid=1000000, puntitot, nreset;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(int giornorr, int meserr, int annorr) {
+        this.data = giornorr+"-"+meserr+"-"+annorr;
+    }
+
+    public String getNomeutente() {
+        return nomeutente;
+    }
+
+    public void setNomeutente(String nomeutente) {
+        this.nomeutente = nomeutente;
+    }
+
+    public String getCognomeutente() {
+        return cognomeutente;
+    }
+
+    public void setCognomeutente(String cognomeutente) {
+        this.cognomeutente = cognomeutente;
+    }
+
+    public int getCodiceid() {
+        return codiceid;
+    }
+
+    public void setCodiceid(int codiceid) {
+        this.codiceid = codiceid;
+    }
+
+    public int getPuntitot() {
+        return puntitot;
+    }
+
+    public void setPuntitot(int puntitot) {
+        this.puntitot = this.puntitot + puntitot;
+    }
+    
+    public boolean elaboropunti(int puntitot){
+        setPuntitot(puntitot);
+        if(this.puntitot>=20){
+            switch(puntitot){
+                case 1:
+                    this.puntitot = this.puntitot - 20;
+                break;
+                case 2:
+                    this.puntitot = this.puntitot - 21;
+                break;
+                case 3:
+                    this.puntitot = this.puntitot - 21;
+                break;               
+            }
+            return true;
+        }
+        else
+            return false;
+    }
+    
+
+    public int getNreset() {
+        return nreset;
+    }
+
+    public void setNreset(int nreset) {
+        this.nreset = nreset;
+    }
+    
+    public Carta(String nomerr){
+        this.nomeutente = nomerr;
+    }
     
 }
--- src/cartagelateria/CartaGelateria.java
+++ src/cartagelateria/CartaGelateria.java
@@ -1,14 +1,79 @@
-
 package cartagelateria;
-
+import java.util.Scanner;
+import ClassiVarie.Carta; //aggiungo la classe carta, che gestisce la tessera in sè per sè
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CartaGelateria {
-
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner kek = new Scanner(System.in);
+    static Random pen = new Random();
+    static ArrayList<Carta> InsiemeCarte = new ArrayList();
+    
+    static String menu(){ //menu che rimanda allo switch nel main
+        
+        String scelta=0;
+        System.out.println("Benvenuto a gelateria Falloni! Solo i Grandi Membri scelgono noi! Seleziona un'opzione: ");
+        int check=0;
+        while(check!=1){
+            System.out.println("1)Creazione tessera \n2)Acquisto di un gelato\n3)Vedi saldo\n 4)Annuario tessere\n 5)Leaderboard \nu)Uscita");
+            if(scelta.equalsIgnoreCase("u"))
+                check=1;
+        }
+        kek.nextLine();       
+        return scelta;
+    }
+    
+    static Carta inserimento(){//inserimento tessera da zero
+        System.out.println("Inserisci il tuo nome: ");
+        Carta tessera = new Carta(kek.nextLine()); //dichiaro un oggetto di tipo Carta
+        System.out.println("Ora il tuo cognome: ");
+        tessera.setCognomeutente(kek.nextLine());
+        while(tessera.getCodiceid()>999999){
+            System.out.println("Inserisci codice carta (6 cifre)");
+            tessera.setCodiceid(kek.nextInt());
+        }
+        return tessera; //ritorno l'oggetto creato, in  modo tale da poterlo mettere in una arraylist
+    }
+    
+    static void acquisto(){
+        String quantitativo="";
+        System.out.println("inserisci codice carta");
+        int codicetemp = kek.nextInt(), check=-1;
+        for(int i=0; i<InsiemeCarte.size(); i++){
+            if(codicetemp == InsiemeCarte.get(i).getCodiceid())
+                check = i;
+        }
+        boolean hovinto=false;
+        if(check!=-1){
+            System.out.println("Quanto Gelato vuoi acquistare? Abbiamo tre quantità: A)0,5kg\nB)0,75kg\nC)1kg");
+            if(quantitativo.equalsIgnoreCase("a"))
+                hovinto =InsiemeCarte.get(check).elaboropunti(1);
+            if(quantitativo.equalsIgnoreCase("b"))
+                hovinto = InsiemeCarte.get(check).elaboropunti(2);
+            if(quantitativo.equalsIgnoreCase("c"))
+                hovinto = InsiemeCarte.get(check).elaboropunti(3);
+        }
+        else
+            System.out.println("codice carta non trovato!");
+        if(hovinto = true)
+            System.out.println("Hai vinto mezzo chilo di gelato gratis! ora smettila di mangiarlo brutta palla di lardo.");
+    }
+    
+    
     public static void main(String[] args) {
-
+        String scelta = menu();
+        
+        switch(scelta){
+            case "1": InsiemeCarte.add(inserimento());
+            break;
+            case "2": acquisto();   
+            break;
+            case "3": System.out.println("inserisci codice");
+                      int temp = kek.nextInt();
+                      for(int i=0; i<InsiemeCarte)
+    }
 
 
 
